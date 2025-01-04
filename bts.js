d4a69fddc0803bf7943a7828abcf66d40ee3cb8f
zjedzonyy <artompor@gmail.com>
2025-01-04 20:44:04 +0100

    Add insert and deleteItem functions

--- bts.js
+++ bts.js
@@ -8,7 +8,70 @@ const Node = (value) => {
 
 const Tree = (array) => {
     let arr = sortAndRemoveDuplicates(array);
-    let root = sortedArrayToBST(arr);
+    let root = sortedArrayToBTS(arr);
+
+    const insert = (value) => {
+        const insertNode = (node, value) => {
+            if (node === null) {
+                return Node(value); 
+            }
+            if (node.data === value) {
+                return node;
+            }
+    
+            if (value < node.data) {
+                node.left = insertNode(node.left, value); 
+            } else if (value > node.data) {
+                node.right = insertNode(node.right, value); 
+            }
+    
+            return node; 
+        };
+
+        root = insertNode(root, value);
+        prettyPrint(root);
+
+    };
+
+    const deleteItem = (value) => {
+        const getSuccessor = (curr) => {
+            curr = curr.right;
+            while (curr !== null && curr.left !== null) {
+                curr = curr.left;
+            }
+            return curr;
+        }
+
+        const deleteNode = (node, value) => {
+            // Empty BST
+            if (node === null) {
+                return node;
+            }
+
+            if (value < node.data) {
+                node.left = deleteNode(node.left, value);
+            } else if (value > node.data) {
+                node.right = deleteNode(node.right, value);
+            } else {
+                // If root matches with the given value
+
+                // Case 1: Has 0 chilldren or only right children
+                if (node.left === null) return node.right;
+
+                // Case 2: Has only left child
+                if (node.right === null) return node.left;
+
+                // Case 3: Has both children
+                let succ = getSuccessor(node);
+                node.data = succ.data;
+                node.right = deleteNode(node.right, succ.data);
+            }
+
+            return node;
+        }
+        root = deleteNode(root, value);
+        prettyPrint(root);
+    }
 
     const prettyPrint = (node, prefix = "", isLeft = true) => {
         if (node === null) {
@@ -25,7 +88,7 @@ const Tree = (array) => {
 
     prettyPrint(root);
 
-    return { arr, root };
+    return { arr, root, insert, deleteItem };
 }
 
 
@@ -46,12 +109,11 @@ const sortAndRemoveDuplicates = (array) => {
     return [...new Set(array)].sort((a, b) => a - b);
 }
 
-const sortedArrayToBST = (arr) => {
+const sortedArrayToBTS = (arr) => {
     return buildTree(arr, 0, arr.length - 1);
 }
 
  
-const ex = Node(1);
 const tree = Tree([5,21,3,1,99,99,2,10,11,12,45,13,86,91,123]);
-// console.log(tree);
+tree.deleteItem(12);
 
